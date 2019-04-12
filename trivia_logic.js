574ca59cb2343ed12bf9883468c1e1797da60f0a
EGS <sweeny.elizabeth@yahoo.com>
2019-04-12 14:02:10 -0700

    Edited files.

---
+++ trivia_logic.js
@@ -0,0 +1,34 @@
+$( document ).ready(function() {
+
+//GLOBAL VARIABLES
+//======================================
+
+
+
+//FUNCTIONS
+//======================================
+
+//Function that initiates the game.
+function startGame(){
+
+}
+
+//Function that serves up a new question.
+function initiateQuestion(){
+
+}
+
+//Function that serves up a new question.
+function timer(){
+
+}
+
+    //Congratulations pop up.
+    document.getElementById("congratulations").style.display = "none";
+    //document.getElementById("congratulations").style.display = "block";
+
+    //Multiple Choice Block
+    document.getElementById("multipleChoiceBlock").style.display = "block";
+
+
+});
