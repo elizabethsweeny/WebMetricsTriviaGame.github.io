d8f2168e0b7d8dde244536ccf50a702d6ae2505b
EGS <sweeny.elizabeth@yahoo.com>
2019-04-13 10:17:52 -0700

    Updating logic.

--- trivia_logic.js
+++ trivia_logic.js
@@ -8,47 +8,60 @@ var totalRounds = 0;
 var totalCorrect = 0;
 var totalWrong = 0;
 //To check whether or not a user has been served a particular question within gameplay - assumes 10 questions total in questionBank.
-var questionTracker = [false, false, false, false, false, false, false, false, false, false];
+//var questionTracker = [false, false, false, false, false, false, false, false, false, false];
 
 //QUESTION BANK
 //======================================
 
 var questionBank = [{
     question: "What is FCP?",
-    correctAnswer: "A field and lab metric that tells when the first pixel was painted on the screen.",
-    wrongAnswers: [
-    "A lab metric that measures when the main thread is quiet enough to respond to user input.", 
-    "Dummy answer 1", 
-    "Dummy answer 2", 
-    "Dummy answer 3", 
-    "Dummy answer 4", 
-    "Dummy answer 5", 
-    "Dummy answer 6"]
+    answers: {
+        a: "A lab metric that measures when the main thread is quiet enough to respond to user input.", 
+        b: "A field and lab metric that tells when the first pixel was painted on the screen.", 
+        c: "A lab measurement of how fast pixels of the page reach their final position.", 
+        d: "The time it takes for the browser to receive the first byte from the server, usually HTML.", 
+    },
+    correctAnswer: "b" 
 },
 {
-    question: "What is TTI?",
-    correctAnswer: "A lab metric that measures when the main thread is quiet enough to respond to user input.",
-    wrongAnswers: [
-    "A field and lab metric that tells when the first pixel was painted on the screen.", 
-    "Dummy answer 1", 
-    "Dummy answer 2", 
-    "Dummy answer 3", 
-    "Dummy answer 4", 
-    "Dummy answer 5", 
-    "Dummy answer 6"]
+    question: "What is Time to Interactive (TTI)?",
+    answers: {
+        a: "A lab metric that measures when the main thread is quiet enough to respond to user input.", 
+        b: "A field and lab metric that tells when the first pixel was painted on the screen.", 
+        c: "A lab measurement of how fast pixels of the page reach their final position.", 
+        d: "The time it takes for the browser to receive the first byte from the server, usually HTML.", 
+    },
+    correctAnswer: "a"  
 },
 {
-    question: "What is First Input Delay (FID)?",
-    correctAnswer: "The time it takes between the user first interacting with the page and the page responding.",
-    wrongAnswers: [
-    "A field and lab metric that tells when the first pixel was painted on the screen.", 
-    "A lab metric that measures when the main thread is quiet enough to respond to user input.",
-    "Dummy answer 1", 
-    "Dummy answer 2", 
-    "Dummy answer 3", 
-    "Dummy answer 4", 
-    "Dummy answer 5", 
-    "Dummy answer 6"]
+    question: "What is Time to First Byte (TTFB)?",
+    answers: {
+        a: "A lab metric that measures when the main thread is quiet enough to respond to user input.", 
+        b: "A field and lab metric that tells when the first pixel was painted on the screen.", 
+        c: "A lab measurement of how fast pixels of the page reach their final position.", 
+        d: "The time it takes for the browser to receive the first byte from the server, usually HTML.", 
+    },
+    correctAnswer: "d"  
+},
+{
+    question: "What is First Meaningful Paint (FMP)?",
+    answers: {
+        a: "A lab measurement of how fast pixels of the page reach their final position.", 
+        b: "A lab and field metric of how long it takes for the “hero” element of the page to become visible", 
+        c: "A field and lab metric that tells when the first pixel was painted on the screen.", 
+        d: "A lab metric that measures when the main thread is quiet enough to respond to user input.", 
+    },
+    correctAnswer: "b"  
+},
+{
+    question: "What is Speed Index?",
+    answers: {
+        a: "A lab metric that measures when the main thread is quiet enough to respond to user input.", 
+        b: "A field and lab metric that tells when the first pixel was painted on the screen.", 
+        c: "A lab measurement of how fast pixels of the page reach their final position.", 
+        d: "The time it takes for the browser to receive the first byte from the server, usually HTML.", 
+    },
+    correctAnswer: "c" 
 }];
 
 //FUNCTIONS
@@ -79,42 +92,82 @@ function startGame(){
 
 
 
-$("startRoundButton").click(function startRound(){
+//$("startRoundButton").click(function startRound()
+
+function startRound(){
     var secondsLeft = 30;
     var interval = setInterval(function () {
         document.getElementById("countdownTimer").innerHTML = --secondsLeft;
         newQuestion(0);
         if (secondsLeft > 0){
-            for (i=0; i<10; i++){
-
-            }
-
+            var i=0;
+            newQuestion(i);
+            var correctAnswer = (questionBank[i].correctAnswer);
+            console.log("Correct answer of given question by index " + correctAnswer);
+            
+            //Finding which answerOptionSpace corresponds to the correct answer.
+                var correctAnswerSpace = "";
+                if (document.getElementById("answerOption1").getAttribute("value") === correctAnswer){
+                    correctAnswerSpace = "answerOption1";
+                }
+                if (document.getElementById("answerOption2").getAttribute("value") === correctAnswer){
+                    correctAnswerSpace = "answerOption2";
+                }
+                if (document.getElementById("answerOption3").getAttribute("value") === correctAnswer){
+                    correctAnswerSpace = "answerOption3";
+                }
+                if (document.getElementById("answerOption4").getAttribute("value") === correctAnswer){
+                    correctAnswerSpace = "answerOption4";
+                }
+                console.log("Correct Answer Space Variable "+ correctAnswerSpace);
             };
 
-            $("#answerOption1").click(function() {
-       
-            });
+            //Checking if which option the user clicked is the correct answer or not.
+            $("#answerOption1, #answerOption2, #answerOption3, #answerOption4").click(function () {
+                if (this.id == correctAnswerSpace) {
+                    alert("You chose the right answer!");
+                    
+                    totalCorrect++;
+                    i++;
+                }
+
+                else if (this.id != correctAnswerSpace) {
+                   alert('Incorrect');
+                }
+             });
 
 
 
         if (secondsLeft <= 0) {
             document.getElementById("countdownTimer").innerHTML = "00";
             clearInterval(interval);
+            //Show alert that time is up, then hide it.
             document.getElementById("timeIsUp").style.display = "block";
+            setTimeout(function() {
+                document.getElementById("timeIsUp").style.display = "none";;
+            }, 10000);
         }
     }, 1000);
-});
+};
+//});
 
 
+
+startRound();
 
 //Function that serves up a new question.
 function newQuestion(questionNumber){
     $("#questionSpace").text(questionBank[questionNumber].question);
+    
     document.getElementById("multipleChoiceBlock").style.display = "block";
-        var randomIncorrect = questionBank[questionNumber].wrongAnswers[Math.floor(Math.random()*10)];
-        $("#answerOption1").text(randomIncorrect);
-    };
-newQuestion(1);
+    $("#answerOption1").text(questionBank[questionNumber].answers.a);
+    $("#answerOption2").text(questionBank[questionNumber].answers.b);
+    $("#answerOption3").text(questionBank[questionNumber].answers.c);
+    $("#answerOption4").text(questionBank[questionNumber].answers.d);
+};
+
+
+
 //TEST BENCH
 //======================================
 
@@ -122,29 +175,46 @@ newQuestion(1);
 // var randomIncorrect = questionBank[0].wrongAnswers[Math.floor(Math.random()*10)];
 // $("#answerOption1").text(randomIncorrect);
 
+//TEST new question, only to be called within start round (DELETE)
+newQuestion(0);
+
+
+//INITIALIZING GAME
+//======================================
+startGame();
+
+
+//SYNTAX REMINDERS
+//======================================
+
 //Syntax for putting a specific question in the Question Space within the HTML.
 //$("#questionSpace").text(questionBank[0].question);
 
-    //Congratulations pop up.
-    document.getElementById("wonRoundAlert").style.display = "none";
-    //document.getElementById("wonRoundAlert").style.display = "block";
+//Congratulations pop up.
+//document.getElementById("wonRoundAlert").style.display = "none";
+//document.getElementById("wonRoundAlert").style.display = "block";
 
-    document.getElementById("correctAnswerAlert").style.display = "none";
-    //document.getElementById("correctAnswerAlert").style.display = "block";
+//document.getElementById("correctAnswerAlert").style.display = "none";
+//document.getElementById("correctAnswerAlert").style.display = "block";
 
-    //Wrong Answer Alert Popup
-    document.getElementById("wrongAnswerAlert").style.display = "none";
-    //document.getElementById("wrongAnswerAlert").style.display = "block";
+//Wrong Answer Alert Popup
+//document.getElementById("wrongAnswerAlert").style.display = "none";
+//document.getElementById("wrongAnswerAlert").style.display = "block";
 
-    //Multiple Choice Block
-    document.getElementById("multipleChoiceBlock").style.display = "none";
-    //document.getElementById("multipleChoiceBlock").style.display = "block";
+//Multiple Choice Block
+//document.getElementById("multipleChoiceBlock").style.display = "none";
+//document.getElementById("multipleChoiceBlock").style.display = "block";
 
-    //Time is up alert.
-    document.getElementById("timeIsUp").style.display = "none";
-    //document.getElementById("timeIsUp").style.display = "block";
+//Time is up alert.
+//document.getElementById("timeIsUp").style.display = "none";
+//document.getElementById("timeIsUp").style.display = "block";
 
-//INITIALIZING GAME
-startGame();
+
+
+//OUT OF SCOPE FOR CURRENT IMPEMENTATION
+//======================================
+
+// Function to show the questions and the correct answers after each round is completed.
+// function showQuestions(){ }
 
 });
